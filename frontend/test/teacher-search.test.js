451dd663a848681a124219adfa3ffa5f7c17bcf6
jonambmp <jonambmp@gmail.com>
2023-10-07 13:36:06 +0530

    Teacher/Student tests .ts files change

--- frontend/test/teacher-search.test.js
+++ frontend/test/teacher-search.test.js
@@ -2,7 +2,7 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:4401/`
+    .page`http://localhost:8080/`
 
 test('Testing search Teachers', async t => {
     await t.navigateTo("/");
@@ -16,3 +16,22 @@ test('Testing search Teachers', async t => {
 
     await t.navigateTo("/dbinitialize");
 });
+
+/* import {Selector} from 'testcafe';
+process.env.NODE_ENV = "test";
+
+fixture`Testing Teacher UI`
+    .page`http://localhost:4401/`
+
+test('Testing search Teachers', async t => {
+    await t.navigateTo("/");
+    await t.typeText("#teacher-search", "su");
+
+    const table = Selector('#teacher-table')
+    const rowCount = await table.find('tr').count;
+
+    let tdText = await table.find('tr').nth(rowCount-1).innerText;
+    await t.expect(rowCount).eql(2)
+
+    await t.navigateTo("/dbinitialize");
+}); */
