e6685156b46b7b6289fada3c353c68a5bb9e7386
jonambmp <jonambmp@gmail.com>
2023-10-18 12:35:09 +0530

    Revert "for db funtion change_1"
    This reverts commit 72632fd0040d787b32b81738767e101566a011ac.

--- backend/database.js
+++ backend/database.js
@@ -28,8 +28,8 @@ const readTeachers = async () => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
-            .then((teacher) => {
-                resolve(teacher);
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
